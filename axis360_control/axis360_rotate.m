d4feed657b5cc99925b1e5ceda2ecf8472b1bfab
Sanjib Sur <sanjib@sanjib-lappy.(none)>
2014-11-27 12:23:35 -0600

    Added the rotator control function

--- axis360_control/axis360_rotate.m
+++ axis360_control/axis360_rotate.m
@@ -5,13 +5,59 @@
 % 
 % Input
 % dev_id: Device specific identifier to controller it through PC
+% axis360: serial obj opened through MATLAB
 % offset_ang: rotating offset from the current position, unit is in deg.
+% reset: To reset the controller
 % 
-% Output: Rotate the Controller
+% Output: Rotates the Controller, return the new state of the obj
 % 
 % Comments: Rotator function for fine-grained control of Axis360.
 % 
 
-function [] = axis360_rotate(dev_id, offset_ang)
+function axis360_out = axis360_rotate(dev_id, axis360, offset_ang, reset, default_params)
+	% If the serial object is not opened, open it first 
+	if isempty(axis360)
+		axis360 = serial(dev_id);
+		set(axis360, 'BaudRate', default_params.baud_rate);
+		fid = fopen(axis360);
+		if fid == -1
+			fprintf('Error in opening the serial object: %s\n',
+								dev_id);
+			return;
+		end
+		axis360.ReadAsyncMode = 'manual';
+		readasync(axis360);
+		% Set the pulse rate
+		fprintf(axis360, sprintf('pr 1 %d\r\n', default_params.pulse_rate));
+		print_serial_data(axis360);
+		fprintf(axis360, sprintf('zm 1\r\n'));	
+		print_serial_data(axis360);
+	end
+	% Reset the motor
+	if reset == 1
+		print_serial_data(axis360);
+                fprintf(axis360, sprintf('zm 1\r\n'));
+		print_serial_data(axis360);
+	else
+		print_serial_data(axis360);
+		% Convert to motor angle
+		angle = round(offset_ang/360*default_params.angle_factor);
+		fprintf(axis360, sprintf('mm 1 %d\r\n', angle));
+		print_serial_data(axis360);
+	end
+	% Assign new state of the serial obj to output
+	axis360_out = axis360;
+end
 
+
+% Prints the serial object data
+function [] = print_serial_data(axis360)
+	if isempty(axis360)
+		fprintf('Serial obj. not ready for communication!\n', axis360);
+		return;
+	end
+	while(axis360.BytesAvailable>0)
+		serialdata = fscanf(axis360);
+		fprintf('%s\n', serialdata);
+	end
 end
