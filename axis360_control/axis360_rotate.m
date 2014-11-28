9a15d9e05d249ef929f3637e68a661c63f08e680
Sanjib Sur <sanjib@sanjib-pc.(none)>
2014-11-27 18:29:17 -0600

    Cinemoco rotator code working

--- axis360_control/axis360_rotate.m
+++ axis360_control/axis360_rotate.m
@@ -7,7 +7,8 @@
 % dev_id: Device specific identifier to controller it through PC
 % axis360: serial obj opened through MATLAB
 % offset_ang: rotating offset from the current position, unit is in deg.
-% reset: To reset the controller
+% reset:  0 - nothing; 1 - to reset the controller; 2 - close the serial
+% obj.
 % 
 % Output: Rotates the Controller, return the new state of the obj
 % 
@@ -19,12 +20,8 @@ function axis360_out = axis360_rotate(dev_id, axis360, offset_ang, reset, defaul
 	if isempty(axis360)
 		axis360 = serial(dev_id);
 		set(axis360, 'BaudRate', default_params.baud_rate);
-		fid = fopen(axis360);
-		if fid == -1
-			fprintf('Error in opening the serial object: %s\n',
-								dev_id);
-			return;
-		end
+		fopen(axis360);
+        % Manual mode reading from the serial obj.
 		axis360.ReadAsyncMode = 'manual';
 		readasync(axis360);
 		% Set the pulse rate
@@ -32,13 +29,17 @@ function axis360_out = axis360_rotate(dev_id, axis360, offset_ang, reset, defaul
 		print_serial_data(dev_id, axis360);
 		fprintf(axis360, sprintf('zm 1\r\n'));	
 		print_serial_data(dev_id, axis360);
+        pause(2);
 	end
 	% Reset the motor
 	if reset == 1
 		print_serial_data(dev_id, axis360);
-                fprintf(axis360, sprintf('zm 1\r\n'));
+        fprintf(axis360, sprintf('zm 1\r\n'));
 		print_serial_data(dev_id, axis360);
-	else
+    elseif reset == 2
+        fclose(axis360);
+        delete(axis360);
+    else
 		print_serial_data(dev_id, axis360);
 		% Convert to motor angle
 		angle = round(offset_ang/360*default_params.angle_factor);
